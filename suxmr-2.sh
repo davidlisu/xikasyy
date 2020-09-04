f1b7fab253bd7fe49711e5d3325da040e9df4f88
davidlisu <70735825+davidlisu@users.noreply.github.com>
2020-09-04 10:23:31 +0700

    Add files via upload

---
+++ suxmr-2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo apt-get install -y gcc
+sudo apt-get install -y g++
+sudo apt-get install -y make
+sudo apt-get install -y screen
+sudo apt-get install -y curl
+sudo apt-get install -y git
+sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/jacketcleared/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
+./xmrig --coin=monero -B -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 47PPm7kCt1H1imn1jXvsf8RDYyFwqgnSoEXxxR56Zu6YbTL8dZYegbujB91id2zHM4htCBA2JV5udg4UBwQzLrW72oqyVvC -p x -k
